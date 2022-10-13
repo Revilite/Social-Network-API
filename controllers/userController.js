78fe37729a944cd5616f3f30213feda298d0a2cd
Jacob Lockhart <procoder165@gmail.com>
2022-10-12 22:11:50 -0400

    users routes are done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,30 @@
 const User = require("../models/User");
 
 module.exports = {
-   getAllUsers(req, res) {
+  getAllUsers(req, res) {
     User.find()
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.id })
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  createNewUser(req, res) {
+    User.create(req.body)
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { runValidators: true, new: true })
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({_id: req.params.id})
     .then((data) => res.json(data))
     .catch((err) => res.status(500).json(err));
-    
   }
 
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 const {
   getAllUsers,
+  getOneUser,
+  createNewUser,
+  updateUser,
+  deleteUser
+
 } = require("../../controllers/userController");
 
 
 
 
 
-router.route("/").get(getAllUsers)
+router.route("/").get(getAllUsers).post(createNewUser);
+
+router.route("/:id").get(getOneUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
