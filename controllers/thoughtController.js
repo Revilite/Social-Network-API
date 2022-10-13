5eaa5dad2a318072d945c154829e6177fe106911
Jacob Lockhart <procoder165@gmail.com>
2022-10-12 22:43:32 -0400

    finished thoughts:

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,30 @@
 const Thought = require("../models/Thought");
 
 module.exports = {
-   getAllThoughts(req, res) {
+  getAllThoughts(req, res) {
     Thought.find()
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  getOneThought(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
     .then((data) => res.json(data))
     .catch((err) => res.status(500).json(err));
-    
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({_id : req.params.id}, {$set: req.body})
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({_id :req.params.id})
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
   }
 
 }
